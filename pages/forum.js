1db6cb3364b8395a007aa6c487b142a2614fca83
notasquid1938 <shoebenfilm@gmail.com>
2023-05-26 00:13:58 -0500

    Improved homepage ui, added images to each webpage button

---
+++ pages/forum.js
@@ -0,0 +1,6 @@
+import { useState, useEffect } from 'react';
+import styles from '../styles/forum.module.css';
+
+export default function Forum() {
+    
+}
--- pages/index.js
+++ pages/index.js
@@ -31,32 +31,40 @@ export default function Home() {
 
       <div className={styles.buttonContainerTop}>
         <Link href="/chat">
-          <a className={`${styles.linkButton} ${styles.blue}`}>Live Chat</a>
+          <a className={`${styles.linkButton} ${styles.buttonColor}`}>
+            <img src="/images/Chat.png" alt="Chat" className={styles.buttonImage} />
+            Live Chat
+          </a>
         </Link>
 
         <Link href="/files">
-          <a className={`${styles.linkButton} ${styles.red}`}>Files</a>
+          <a className={`${styles.linkButton} ${styles.buttonColor}`}>
+            <img src="/images/Files.png" alt="Files" className={styles.buttonImage} />
+            Files
+          </a>
         </Link>
       </div>
 
       <div className={styles.buttonContainerBottom}>
         <Link href="/blog">
-          <a className={`${styles.linkButton} ${styles.green}`}>Blog</a>
+          <a className={`${styles.linkButton} ${styles.buttonColor}`}>
+            <img src="/images/Logo.png" alt="Blog" className={styles.buttonImage} />
+            Blog
+          </a>
         </Link>
 
         <Link href="/forum">
-          <a className={`${styles.linkButton} ${styles.blue}`}>Forum</a>
+          <a className={`${styles.linkButton} ${styles.buttonColor}`}>
+            <img src="/images/Forum.jpg" alt="Forum" className={styles.buttonImage} />
+            Forum
+          </a>
         </Link>
       </div>
 
       <div>
         <h1>About</h1>
         <p1 className="About">
-          The website aims to combat money&apos;s overwhelming influence in politics and challenge corporate conglomerates. We believe that a fair and just society can only be achieved by empowering people to organize and advocate for change. Our mission is to provide a platform that enables individuals to come together, share knowledge, and take collective action for the benefit of all.
-
-          Through our various resources, such as blogs, files, and chat functionality, we strive to create a space where individuals can educate themselves, engage in meaningful discussions, and organize grassroots movements. We encourage active participation, collaboration, and the exchange of ideas to promote transparency, accountability, and social justice.
-
-          Join us in our fight against the undue influence of money in politics and the growing power of corporate conglomerates. Together, we can make a difference and work towards a more equitable and inclusive society, where the needs and well-being of all individuals are prioritized.
+          Lorem ipsum... you get the idea. This needs work.
         </p1>
       </div>
     </div>
