b2f8d7965b8470ad59b32b11eda19912e8b4235d
notasquid1938 <shoebenfilm@gmail.com>
2023-05-30 23:15:37 -0500

    Added the groundwork of a translation feature, only works on homepage

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,25 @@
+import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
 import Head from 'next/head';
 import Link from 'next/link';
 import styles from '../styles/index.module.css';
+import i18n from '../i18n';
 
 export default function Home() {
+  const { t } = useTranslation();
+  const [selectedLanguage, setSelectedLanguage] = useState('en');
+
+  const handleLanguageChange = (event) => {
+    const language = event.target.value;
+    setSelectedLanguage(language);
+    i18n.changeLanguage(language);
+  };
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Homepage</title>
-        <meta name="description" content="A grassroots community dedicated to combating corporate conglomerates." />
+        <title>{t('homepage.title')}</title>
+        <meta name="description" content={t('homepage.description')} />
       </Head>
 
       <div className={styles.githubButtonContainer}>
@@ -22,25 +34,34 @@ export default function Home() {
       </div>
 
       <div className={styles.title}>
-        <h1>Welcome to the Homepage!</h1>
+        <h1>{t('homepage.welcome')}</h1>
       </div>
 
       <div className={styles.slogan}>
-        <p1>For the Benefit of All</p1>
+        <p1>{t('homepage.slogan')}</p1>
+      </div>
+
+      <div className={styles.languageDropdown}>
+        <select value={selectedLanguage} onChange={handleLanguageChange}>
+          <option value="en">English</option>
+          <option value="es">Spanish</option>
+          <option value="fr">French</option>
+          {/* Add more language options as needed */}
+        </select>
       </div>
 
       <div className={styles.buttonContainerTop}>
         <Link href="/chat">
           <a className={`${styles.linkButton} ${styles.buttonColor}`}>
             <img src="/images/Chat.png" alt="Chat" className={styles.buttonImage} />
-            Live Chat
+            {t('homepage.liveChat')}
           </a>
         </Link>
 
         <Link href="/files">
           <a className={`${styles.linkButton} ${styles.buttonColor}`}>
             <img src="/images/Files.png" alt="Files" className={styles.buttonImage} />
-            Files
+            {t('homepage.files')}
           </a>
         </Link>
       </div>
@@ -49,23 +70,21 @@ export default function Home() {
         <Link href="/blog">
           <a className={`${styles.linkButton} ${styles.buttonColor}`}>
             <img src="/images/Logo.png" alt="Blog" className={styles.buttonImage} />
-            Blog
+            {t('homepage.blog')}
           </a>
         </Link>
 
         <Link href="/forum">
           <a className={`${styles.linkButton} ${styles.buttonColor}`}>
             <img src="/images/Forum.png" alt="Forum" className={styles.buttonImage} />
-            Forum
+            {t('homepage.forum')}
           </a>
         </Link>
       </div>
 
       <div>
-        <h1 className={styles.aboutTitle}>About</h1>
-        <p1 className="About">
-          Lorem ipsum... you get the idea. This needs work.
-        </p1>
+        <h1 className={styles.aboutTitle}>{t('homepage.aboutTitle')}</h1>
+        <p1 className="About">{t('homepage.aboutContent')}</p1>
       </div>
     </div>
   );
